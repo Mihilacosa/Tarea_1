8085428ab3deaac1d04605e9598e4b8830a2a29a
Mihilacosa <mhosliag01@educantabria.es>
2020-10-25 17:43:13 +0100

    Menu

--- app/src/main/java/com/example/tarea_1/MainActivity.java
+++ app/src/main/java/com/example/tarea_1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tarea_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu mimenu){
+
+        getMenuInflater().inflate(R.menu.menu_activity, mimenu);
+
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem opciones_menu){
+
+        int id = opciones_menu.getItemId();
+
+        if(id == R.id.login){
+
+            return true;
+        }
+
+        if(id == R.id.registro){
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(opciones_menu);
     }
 }
